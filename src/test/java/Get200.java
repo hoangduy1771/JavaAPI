17110ef0bba8c9cf352ee73753d79473cef83a27
hoang duy <hoangduy1771@gmail.com>
2022-05-02 22:13:17 +0700

    add first test

---
+++ src/test/java/Get200.java
@@ -0,0 +1,25 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Get200 {
+    public static final String BASE_ENDPOINT = "https://api.github.com/";
+    HttpClient client = HttpClientBuilder.create().build();
+
+    @Test
+    public void baseUrlReturns200() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT);
+
+        HttpResponse response = client.execute(get);
+        int actualStatus = response.getStatusLine().getStatusCode();
+
+        Assert.assertEquals(200, actualStatus);
+
+    }
+
+}
