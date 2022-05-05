7778ffb1e38750ced83b88aadd0f9f12566df97c
Hoang Duy <hoangduy1771@gmail.com>
2022-05-05 18:40:44 +0700

    add test

--- src/test/java/BodyTestWithMap.java
+++ src/test/java/BodyTestWithMap.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class BodyTestWithMap {
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.swing.text.html.parser.Entity;
+import java.io.IOException;
+
+
+public class BodyTestWithMap extends BaseClass {
+    CloseableHttpClient client;
+    CloseableHttpResponse response;
+
+    @BeforeMethod
+    public void setUp() {
+        HttpHost proxy = new HttpHost("rb-proxy-unix-apac.bosch.com", 8080);
+        client = HttpClientBuilder.create().setProxy(proxy).build();
+        // client = HttpClientBuilder.create().build();
+
+    }
+
+    @AfterMethod
+    public void closeResources() throws IOException {
+        client.close();
+        response.close();
+    }
+
+    @Test
+    public void returnsCorrectLogin() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "user/hoangduy1771@gmail.com");
+        response = client.execute(get);
+        String jsonBody = EntityUtils.toString(response.getEntity());
+
+        System.out.println(jsonBody);
+
+    }
+
 }
